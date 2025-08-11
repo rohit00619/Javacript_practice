d95c7140b1bc3c23073fdacf91994f45a645d8ca
Rohit chandoriya <rohit00619@gmail.com>
2025-08-11 20:47:03 +0530

    change the color to Purple with a button click.
    change the color to Purple with a button click.

---
+++ js.js
@@ -0,0 +1,12 @@
+var inputcolor1=document.getElementById("inputcolor").name;
+
+console.log("inputcolor1")
+
+ function changecolorfn1(){
+    document.getElementById ('div1').style.backgroundColor="purple";
+ }
+
+ function changecolorfn(){
+    var inp=document.getElementById("inputcolor").name;
+    console.log(inp);
+ }
